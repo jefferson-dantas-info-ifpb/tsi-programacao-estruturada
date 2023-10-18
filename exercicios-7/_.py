fe01d6ee0ecf0cf6ad9a9f4c44d9c934d7c95d2e
Jefferson Dantas <52979246+josejefferson@users.noreply.github.com>
2023-10-18 16:59:02 +0000

    Criado as funções custom

---
+++ exercicios-7/_.py
@@ -0,0 +1,14 @@
+    # comeco = None
+    # fim = None
+    # for i, letra in range(len_custom(string) - len(substring_antiga)):
+    #     if letra == substring_antiga[0]: # inicio da substring
+    #         for j, substring_letra in enumerate(substring_antiga): # testa se tem a substring completa
+    #             if string[i + j] != substring_letra:
+    #                 break
+    #             if j == len_custom(substring_antiga) - 1: # última letra da substring
+                   
+        # for j, lletra in enumerate(substring_antiga):
+        #     if letra != lletra:
+        #         continue
+        #     else:
+        #         comeco = i
--- exercicios-7/main.py
+++ exercicios-7/main.py
@@ -33,45 +33,104 @@ def calcular_pagamento(horas_trabalhadas, taxa_hora):
     return horas_trabalhadas * taxa_hora
 
 def maior_numero(a, b, c):
-    return max(a, b, c)
+    return max_custom([a, b, c])
 
 def contagem_letras(palavra):
-    totalMaiuscula = 0
-    totalMinuscula = 0
+    total_maiuscula = 0
+    total_minuscula = 0
 
     for letra in palavra:
         if letra in 'abcdefghijklmnopqrstuvwxyz':
-            totalMinuscula += 1
+            total_minuscula += 1
         elif letra in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
-            totalMaiuscula += 1
+            total_maiuscula += 1
     
-    return (totalMaiuscula, totalMinuscula)
+    return (total_maiuscula, total_minuscula)
 
 def len_custom(iteravel):
-    return len(iteravel)
+    contagem = 0
+    for _ in iteravel:
+        contagem += 1
+    return contagem
 
 def sum_custom(lista):
-    return sum(lista)
+    soma = 0
+    for numero in lista:
+        soma += numero
+    return soma
 
 def max_custom(lista):
-    if len(lista) != 0:
-        return max(lista)
+    maior = None
+
+    for numero in lista:
+        if maior is None or numero > maior:
+            maior = numero
+
+    return maior
 
 def min_custom(lista):
-    if len(lista) != 0:
-        return min(lista)
+    menor = None
+
+    for numero in lista:
+        if menor is None or numero < menor:
+            menor = numero
+
+    return menor
 
 def startswith_custom(string, prefixo):
-    return string.startswith(prefixo)
+    indice = 0
+    for caractere in prefixo:
+        if caractere != string[indice]:
+            return False
+        indice += 1
+    return True
 
 def endswith_custom(string, prefixo):
-    return string.endswith(prefixo)
+    indice = len_custom(string) - 1
+    for caractere in reversed(prefixo):
+        if caractere != string[indice]:
+            return False
+        indice -= 1
+    return True
 
 def split_custom(string, caractere):
-    return string.split(caractere)
+    resultado = []
+
+    parte = ''
+    for letra in string:
+        if letra != caractere:
+            parte += letra
+        else:
+            resultado.append(parte)
+            parte = ''
+    resultado.append(parte)
+    parte = ''
+
+    return resultado
 
 def strip_custom(string, caracteres_remover):
-    return string.strip(caracteres_remover)
+    resultado = string
+    while True:
+        if startswith_custom(resultado, caracteres_remover):
+            resultado = resultado[1:]
+        elif endswith_custom(resultado, caracteres_remover):
+            resultado = resultado[:-1]
+        else:
+            break
+
+    return resultado
 
 def replace_custom(string, substring_antiga, substring_nova):
-    return string.replace(substring_antiga,substring_nova)
+    pedacos_da_string = string.split(substring_antiga)
+    resultado_em_pedacos = []
+    print(pedacos_da_string)
+
+    for i in range(len_custom(pedacos_da_string)):
+        resultado_em_pedacos.append(pedacos_da_string[i])
+        if i != len_custom(pedacos_da_string) - 1:
+            resultado_em_pedacos.append(substring_nova)
+    resultado = ''
+    for pedaco in resultado_em_pedacos:
+        resultado += pedaco
+
+    return resultado
